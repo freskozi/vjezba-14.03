11d1fdfbf323fe243b27126c52606cfc98e2f94d
freskozi <denisbandic98@gmail.com>
2023-03-14 19:29:57 +0100

    gfh

---
+++ Primjer.js
@@ -0,0 +1,9 @@
+var sc = require("./StepCounter");
+
+sc.walk();
+console.log(sc.getTotalSteps());
+
+sc.walk(); 
+sc.walk(); 
+sc.walk(); 
+console.log(sc.getTotalSteps());
---
+++ StepCounter.js
@@ -0,0 +1,12 @@
+var StepCounter = {
+    koraci: 0,
+    walk: function () {
+        this.koraci++;
+    
+    },
+    getTotalSteps: function () {
+        return this.koraci;
+    }
+};
+
+module.exports = StepCounter;
